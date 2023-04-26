98af94fd397462be0a8b86cc16451af475ad0211
Lorne Chen <laurenchen0631@gmail.com>
2023-04-26 11:46:17 -0700

    rust multithread webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,15 +1,25 @@
 use std::{
     fs,
     net::{TcpListener, TcpStream},
-    io::{BufReader, prelude::*},
+    io::{BufReader, prelude::*}, 
+    thread, 
+    time::Duration,
 };
+
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        handle_connection(&mut stream);
+        pool.execute(move || {
+            handle_connection(&mut stream);
+        });
+
+        // let mut stream = stream.unwrap();
+        // handle_connection(&mut stream);
         
         println!("Connection established!");
     }
@@ -25,10 +35,14 @@ fn handle_connection(mut stream: &TcpStream) {
     //     .collect();
     let request_line = reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "static/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "static/index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
