f1c0a57dd493de6d4bafb4fc58a61ebd92b8ae20
Lorne Chen <laurenchen0631@gmail.com>
2023-04-26 10:31:45 -0700

    single thread webserver

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,40 @@
+use std::{
+    fs,
+    net::{TcpListener, TcpStream},
+    io::{BufReader, prelude::*},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        handle_connection(&mut stream);
+        
+        println!("Connection established!");
+    }
+}
+
+
+fn handle_connection(mut stream: &TcpStream) {
+    let reader = BufReader::new(stream);
+    // let request: Vec<_> = reader
+    //     .lines()
+    //     .map(|line| line.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "static/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write(response.as_bytes()).unwrap();
 }
