765fc5420966bd9cced21e4406eed2f0e1ad13da
Lorne Chen <laurenchen0631@gmail.com>
2023-05-02 12:02:02 -0700

    wasm template

--- wasm-react/next.config.js
+++ wasm-react/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const path = require('path');
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
 
 module.exports = {
   reactStrictMode: true,
@@ -6,7 +16,21 @@ module.exports = {
     appDir: true,
   },
   webpack: function (config, options) {
-      config.experiments = { asyncWebAssembly: true };
+      config.experiments = {
+        asyncWebAssembly: true,
+        layers: true,
+      };
+
+      config.module.rules.push({
+        test: /\.wasm$/,
+        type: 'webassembly/async',
+      });
+
+      config.plugins.push(
+        new WasmPackPlugin({
+          crateDirectory: path.resolve(__dirname, 'wasm'),
+        })
+      );
       return config;
   }
 }
